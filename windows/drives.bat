94cddbde32a70f92082b4a06bc612babffb2698f
Russell de Moose <moosamus@gmail.com>
2013-06-04 15:27:07 -0400

    these scripts should go in the exhibit-specific repos. oops.

--- windows/drives.bat
+++
@@ -1,6 +0,0 @@
-net use M: \\multicam1\share /persistent:y
-net use N: \\multicam2\share /persistent:y
-net use O: \\multicam3\share /persistent:y
-net use P: \\multicam4\share /persistent:y
-net use R: \\multicam5\share /persistent:y
-net use S: \\multicam6\share /persistent:y
